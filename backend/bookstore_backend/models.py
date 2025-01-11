65d4dbbf2e0e22041639091e29a8ae7296b602c4
sandeshpd <sandesh@sandesh.com>
2025-01-11 21:53:34 +0530

    the backend of the project

---
+++ backend/bookstore_backend/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=25)
+    language = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
---
+++ backend/bookstore_backend/urls.py
@@ -0,0 +1,8 @@
+# from django.urls import path
+# from rest_framework.routers import DefaultRouter
+# from .views import BookView
+
+# router = DefaultRouter()
+# router.register(r'books', BookView, basename='books')
+
+# urlpatterns = router.urls
