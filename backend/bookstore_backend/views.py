88cf6536e5c9c367e297eab2af2a46cc50e7eee5
sandeshpd <sandesh@sandesh.com>
2025-01-13 19:19:02 +0530

    removed restriction
    anyone can perform CRUD operation as it was not intended

--- backend/bookstore_backend/views.py
+++ backend/bookstore_backend/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import *
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from .serializer import *
 
 # Create your views here.
 class BookView(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
-    permission_classes = [AllowAny]
