5979829389f53cc7b47000b16a0b0063757d6fac
sandeshpd <sandesh@sandesh.com>
2025-01-13 19:21:39 +0530

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,56 +2,32 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
+import BookList from './components/List';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 class App extends React.Component {
+  // constructor to set redirectToList to false initially
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirectToList: false,
+    };
+  }
+
   state = {
     details: [],
     name: "",
     author: "",
     language: "",
+    genre: "",
   };
-
-  componentDidMount() {
-    let data;
-
-    // Get the books data from the backend
-    axios
-    .get('/api/books/', {
-      headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM2NTc5NjMzLCJpYXQiOjE3MzY1NzkzMzMsImp0aSI6ImIyMzAyMjQzMmU1MDQzMjJiYWYxZDFkMGE4N2E2Mjc2IiwidXNlcl9pZCI6MX0.DfatMUf7qBnnDGhp0_3AUWGII0c9pTrReMLJ_suY3Tw`,
-      },
-    })
-    .then(res => {
-      data = res.data;
-      this.setState({
-        details : data
-      });
-    })
-    .catch(err => console.log(err));
-  }
-
-  refreshList() {
-    axios
-    .get('/api/books/', {
-        headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM2NTc5NjMzLCJpYXQiOjE3MzY1NzkzMzMsImp0aSI6ImIyMzAyMjQzMmU1MDQzMjJiYWYxZDFkMGE4N2E2Mjc2IiwidXNlcl9pZCI6MX0.DfatMUf7qBnnDGhp0_3AUWGII0c9pTrReMLJ_suY3Tw`,
-        },
-      }
-    )
-    .then((res) => {
-      console.log(res.data)
-      this.setState({ details:res.data })
-    })
-    .catch((err) => console.log(err));
-  };
-
 
   handleInput = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
     });
   };
-  
+
   // Post the books data to the backend
   handleSubmit = (e) => {
     e.preventDefault();
@@ -63,106 +39,85 @@ class App extends React.Component {
       name: this.state.name,
       author: this.state.author,
       language: this.state.language,
+      genre: this.state.genre,
     })
-    .then((res) => {
-      this.setState({
-        name: '',
-        author: '',
-        language: '',
-      });
-    })
-    .catch((err) => console.log(err));
+      .then((res) => {
+        this.setState({
+          name: '',
+          author: '',
+          language: '',
+          genre: '',
+        });
+      })
+      .catch((err) => console.log(err));
   }
 
-  // Delete particular book from the backend
-  handleDelete = (book) => {
-    axios
-    .delete(`/api/books/${book.id}/`)
-    .then((res) => this.refreshList());
+  handleRedirect = () => {
+    this.setState({ redirectToList: true });
+    //console.log('working fine')
   };
 
-  render() {
-    return (
-      <div className="container jumbotron ">
-          <form onSubmit={this.handleSubmit}>
-              <div className="input-group mb-3">
-                  <div className="input-group-prepend">
-                      <span className="input-group-text"
-                            id="basic-addon1">
-                              Name
-                      </span>
-                  </div>
-                  <input type="text" className="form-control" 
-                         placeholder="Name of the Book"
-                         aria-label="Book name"
-                         aria-describedby="basic-addon1"
-                         value={this.state.name} name="name"
-                         onChange={this.handleInput} />
-              </div>
-    
-              <div className="input-group mb-3">
-                  <div className="input-group-prepend">
-                      <span className="input-group-text">
-                         Book data 
-                      </span>
-                  </div>
-                  <input type='text' className="form-control" 
-                            aria-label="Author name"
-                            placeholder="Who wrote this?" 
-                            value={this.state.author} name="author" 
-                            onChange={this.handleInput}>
-                  </input>
 
-                  <input type='text' className="form-control" 
-                            aria-label="Language"
-                            placeholder="In what language?" 
-                            value={this.state.language} name="language" 
-                            onChange={this.handleInput}>
-                  </input>
-              </div>
-    
-              <button type="submit" className="btn btn-primary mb-5">
-                  Submit
-              </button>
+  // Navigate to Book collection page
+  // FIXME: Collection must be displayed immediately after going to /booklist page
+  render() {
+    if (this.state.redirectToList) {
+      return <Navigate to="/booklist" />;
+    }
+
+    return (
+        <div className="container jumbotron ">
+          <form onSubmit={this.handleSubmit}>
+            <div className="input-group mb-3">
+              <md-outlined-text-field type="text"
+                className="bookdata-textfield"
+                placeholder="Name of the Book"
+                aria-describedby="basic-addon1"
+                value={this.state.name} name="name"
+                onChange={this.handleInput} />
+            </div>
+
+            <div className="input-group mb-3">
+              <md-outlined-text-field type='text'
+                className="bookdata-textfield"
+                placeholder="Who wrote this?"
+                value={this.state.author} name="author"
+                onChange={this.handleInput}>
+              </md-outlined-text-field>
+
+              <md-outlined-text-field type='text'
+                className="bookdata-textfield"
+                placeholder="In what language?"
+                value={this.state.language} name="language"
+                onChange={this.handleInput}>
+              </md-outlined-text-field>
+
+              <md-outlined-text-field type='text'
+                className="bookdata-textfield"
+                placeholder="What's this book about?"
+                value={this.state.language} name="language"
+                onChange={this.handleInput}>
+              </md-outlined-text-field>
+            </div>
+
+            <button type="submit" className="btn btn-primary mb-5">
+              Submit
+            </button>
           </form>
 
-          Count of Books: {this.state.details.length}
-          <hr
-              style={{
-                  color: "#000000",
-                  backgroundColor: "#000000",
-                  height: 0.5,
-                  borderColor: "#000000",
-              }}
-          />
-    
-          {this.state.details.map((detail, id) => (
-              <div key={id}>
-                  <div className="card shadow-lg">
-                      <div className="card-body">
-                        <div className="deleteItem">
-                          {/* FIXME: Pass id/reference of the object (in this context, book)
-                           to be deleted in handleDelete() function */}
-                          <button 
-                            className='deleteBtn'
-                            type='button' 
-                            onClick={ () => this.handleDelete() }>
-                            <span className="material-symbols-rounded">
-                              delete_forever
-                            </span>
-                          </button>
-                        </div>
-                            <h1> {detail.detail} </h1>
-                            <h4 className='Bookdata'>{detail.name}</h4>
-                            <h4 className='Bookdata'>{detail.author}</h4>
-                            <h4 className='Bookdata'>{detail.language}</h4>
-                      </div>
-                  </div>
-                  <span className="border border-primary"></span>
-              </div>
-          ))}
-      </div>
-    );    
+          <div>
+            <button
+              onClick={ this.handleRedirect }
+              className='btn btn-primary'>
+              View Collection
+            </button>
+          </div>
+        <Routes>
+          <Route path="/booklist" element={<BookList />} />
+        </Routes>
+        </div>
+
+    );
   }
 }
 
