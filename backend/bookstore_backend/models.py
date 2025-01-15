b777137fa7b491ce53bae884152e01d6bc773056
sandeshpd <sandesh@sandesh.com>
2025-01-15 23:08:58 +0530

    added new field genre

--- backend/bookstore_backend/models.py
+++ backend/bookstore_backend/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    name = models.CharField(max_length=50)
-    author = models.CharField(max_length=25)
-    language = models.CharField(max_length=10)
+    name = models.CharField(max_length = 50)
+    author = models.CharField(max_length = 25)
+    language = models.CharField(max_length = 10)
+    genre = models.CharField(max_length = 20, null = True, blank = True)
 
     def __str__(self):
         return self.name
