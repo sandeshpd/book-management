959ffd222f4b1deba36dc4f76a09471d195c0074
sandeshpd <sandesh@sandesh.com>
2025-01-19 22:48:46 +0530

    added functionality to edit books

--- frontend/src/components/List/List.js
+++ frontend/src/components/List/List.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import '../../App.js';
+import EditDialog from '../Dialog/Dialog.js';
 import axios from 'axios';
 
 function BookList() {
     const [details, setDetails] = useState([]);
+    const [isDialogOpen, setDialogOpen] = useState(false);
+    const [selectedBook, setSelectedBook] = useState(null);
+
+    /*const state = {
+        details: [],
+        isEdited: false,
+        currentBook: { id: '', name: '', author: '', language: '', genre: '' },
+    };*/
 
     useEffect(() => {
         //Get the books data from the backend
@@ -14,6 +23,35 @@ function BookList() {
             })
             .catch(err => console.log(err));
     }, []);
+
+    // Open dialog box
+    const openEditingDialog = (book) => {
+        console.log('opening dialog for editing book', book);
+        setSelectedBook(book);
+        setDialogOpen(true);
+    };
+
+    // Close dialog box
+    const closeEditingDialog = () => {
+        setSelectedBook(null);
+        setDialogOpen(false);
+    };
+
+    // Update book in backend and update state
+    const updateBook = (updatedBook) => {
+        axios
+            .patch(`/api/books/${updatedBook.id}/`, updatedBook)
+            .then((res) => {
+                console.log('Book updated: ', res.data);
+                const updatedList = details.map((book) => 
+                    book.id === updatedBook.id ? res.data : book
+                );
+                console.log('updated list of books:', updatedList)
+                setDetails(updatedList);
+                closeEditingDialog();
+            })
+            .catch((err) => console.log('Error updating book info:', err));
+    };
 
     // Delete book associated with given id from the backend
     const handleDelete = (id) => {
@@ -72,8 +110,7 @@ function BookList() {
                                     <button
                                         className='edit-btn'
                                         type='button'
-                                    // onClick={handleEdit}
-                                    >
+                                        onClick={() => openEditingDialog(book)}>
                                         <span className='material-symbols-rounded'>
                                             edit
                                         </span>
@@ -99,8 +136,19 @@ function BookList() {
                     </div>
                 </div>
             ))}
+            {isDialogOpen && selectedBook ? (
+                <div className='dialog-main-container'>
+                    <EditDialog
+                        book={selectedBook}
+                        onClose={closeEditingDialog}
+                        onSave={updateBook}
+                    />
+                </div>
+            ) : (
+                <></>
+            )}
         </div>
     );
-}
+};
 
 export default BookList;
