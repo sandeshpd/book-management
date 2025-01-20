a268698b06ae08546b758b72c89af2f250a9e1d4
sandeshpd <sandesh@sandesh.com>
2025-01-20 16:27:42 +0530

    added editing feature and some styling

--- frontend/src/components/Dialog/Dialog.js
+++ frontend/src/components/Dialog/Dialog.js
@@ -20,60 +20,63 @@ const EditDialog = ({ book, onClose, onSave }) => {
 
   return (
     /* Add Material Design 3 Dialog box here */
-    <>
-      <div className="edit-form-container">
-        <md-dialog open>
-          <span slot="headline">
-            <md-icon-button form='form' value='close' aria-label='Close dialog'>
-              <md-icon>close</md-icon>
-            </md-icon-button>
-            <span className="headline">Edit Book Information</span>
-          </span>
-          <form
-            id="form"
-            slot="content"
-            method="post"
-            className="bookinfo-content">
-            <div className="contact-row">
-              <md-outlined-text-field
-                className='editDialog-textfield'
-                autofocus
-                label='Name'
-                onChange={ handleInputChange }></md-outlined-text-field>
-              <md-outlined-text-field
-                className='editDialog-textfield'
-                autofocus
-                label='Author'
-                onChange={ handleInputChange }></md-outlined-text-field>
-              <md-outlined-text-field
-                className='editDialog-textfield'
-                autofocus
-                label='Language'
-                onChange={ handleInputChange }></md-outlined-text-field>
-              <md-outlined-text-field
-                className='editDialog-textfield'
-                autofocus
-                label='Genre'
-                onChange={ handleInputChange }></md-outlined-text-field>
+    <div className="edit-book-form-container">
+      <md-dialog
+        open
+        className='dialog-box'>
+        <span slot="headline">
+          <span className="headline">Edit Book Information</span>
+        </span>
+        <form
+          id="form"
+          slot="content"
+          method="post"
+          className="bookinfo-content">
+          <div className="edit-text-fields-row">
+            <md-outlined-text-field
+              className='editDialog-textfield'
+              name='name'
+              autofocus
+              label='Name'
+              onChange={handleInputChange}></md-outlined-text-field>
+            <md-outlined-text-field
+              className='editDialog-textfield'
+              name='author'
+              autofocus
+              label='Author'
+              onChange={handleInputChange}></md-outlined-text-field>
+            <md-outlined-text-field
+              className='editDialog-textfield'
+              name='language'
+              autofocus
+              label='Language'
+              onChange={handleInputChange}></md-outlined-text-field>
+            <md-outlined-text-field
+              className='editDialog-textfield'
+              name='genre'
+              autofocus
+              label='Genre'
+              onChange={handleInputChange}></md-outlined-text-field>
 
-              {/* Save and Cancel buttons */}
+            {/* Save and Cancel buttons */}
+            <div className="buttons-container">
               <md-filled-button
                 value="save"
                 className='save-edited-info'
-                onClick={ handleSave }>
+                onClick={handleSave}>
                 Save
               </md-filled-button>
               <md-filled-button
                 value="close"
                 className='cancel-edited-info'
-                onClick={ onClose }>
+                onClick={onClose}>
                 Cancel
               </md-filled-button>
             </div>
-          </form>
-        </md-dialog>
-      </div>
-    </>
+          </div>
+        </form>
+      </md-dialog>
+    </div>
   );
 };
 
