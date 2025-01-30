8abcddfdf0cda8acc686bd1081beacf207daa8fd
sandeshpd <sandesh@sandesh.com>
2025-01-30 16:35:09 +0530

    increased max_length

--- backend/bookstore_backend/models.py
+++ backend/bookstore_backend/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     name = models.CharField(max_length = 50)
-    author = models.CharField(max_length = 25)
-    language = models.CharField(max_length = 10)
-    genre = models.CharField(max_length = 20, null = True, blank = True)
+    author = models.CharField(max_length = 50)
+    language = models.CharField(max_length = 20)
+    genre = models.CharField(max_length = 50, null = True, blank = True)
 
     def __str__(self):
         return self.name
